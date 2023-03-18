d7728fa4daec07cf46cf0cc56e2c5e65a3586298
Bryce Meadors <bryce.meadors@gmail.com>
2023-03-17 17:22:59 -0700

    Added functionality to add task button, added default background image

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const unsplash_api_url = "https://api.unsplash.com/photos/";
 const unsplash_access_key = "YjSH_enuPwnF_m61VtLbNdmyI12J5DtWCJHbyq7iJ8c";
 const quote_element = document.getElementById("quote");
 const add_task_button = document.getElementById("add_task_button");
-let background = document.body.background;
+const default_bg = "./default_bg.png";
+var user_daily_streak = 0;
+var task_counter = 0;
 
 const months = {
   1: "January",
@@ -25,20 +27,20 @@ document.onload = setUp();
 
 /* Method for setting up the webpage */
 async function setUp() {
-  todaysDate();
-  trackStreak();
+  todays_date();
+  track_streak();
   fill_quote_element(await getAPI(quotes_api_url));
-  // fill_background(
-  //   await getAPI(
-  //     `${unsplash_api_url}?query=motivational&count=${number_of_photos}&client_id=${unsplash_access_key}`
-  //   )
-  // );
+  fill_background(
+    await getAPI(
+      `${unsplash_api_url}?query=pastel&count=${number_of_photos}&client_id=${unsplash_access_key}`
+    )
+  );
   get_player_exp();
   get_player_level();
 }
 
 /* Method for getting the date */
-function todaysDate() {
+function todays_date() {
   const header = document.getElementById("header");
   let fullDate = new Date();
   let date = fullDate.getDate();
@@ -50,9 +52,8 @@ function todaysDate() {
 }
 
 /* Method for tracking how many days in a row the user has logged in */
-function trackStreak() {
+function track_streak() {
   // track daily streak somewhere
-  var user_daily_streak = 0;
   user_daily_streak++;
   const task_streak_counter = document.getElementById("streak_counter");
   // if user has a day streak greater than 1, add an s
@@ -62,8 +63,43 @@ function trackStreak() {
   `;
 }
 
+/* Method for adding functionality to the checkbox items in the to-do list */
+function update_progress_bar() {}
+
 add_task_button.onclick = function () {
-  /* Create a form to fill out, make it a task */
+  // Create a form to fill out
+  const html_to_add = `
+  <form id="task_form">
+    <div class="form-group">
+      <label for="task_form">What task do you want to add to your to-do list?</label>
+      <input type="text" class="form-control" id="task" placeholder="Example: Do the dishes">
+    </div>
+  </form>
+  `;
+  // Add form to section below button
+  document.getElementById("form_div").innerHTML = html_to_add;
+
+  const task_form = document.getElementById("task_form");
+  const to_do_list = document.getElementById("to_do_list");
+  const task = document.getElementById("task");
+  // When submit event happens, take user input and add it to the to-do list
+  task_form.addEventListener("submit", (event) => {
+    task_counter++;
+    to_do_list.innerHTML += `
+    <div class="form-check">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="item_${task_counter}">
+          <label class="form-check-label" for="item_${task_counter}">
+          ${task.value}
+          </label>
+        </div>
+    `;
+    task_form.style.display = "none";
+    event.preventDefault();
+  });
 };
 
 /* Method for getting the quote of the day */
@@ -80,11 +116,15 @@ function fill_quote_element(json) {
   `;
 }
 
+/* Method for changing the background */
 function fill_background(json) {
   var index = Math.floor(Math.random() * number_of_photos);
-  const background_img_url = json[index].urls.full;
-  console.log(background_img_url);
-  document.body.style.backgroundImage = "url('" + background_img_url + "')";
+  try {
+    const background_img_url = json[index].urls.full;
+    document.body.style.backgroundImage = "url('" + background_img_url + "')";
+  } catch (undefined) {
+    document.body.style.backgroundImage = "url('" + default_bg + "')";
+  }
 }
 
 /* Method for tracking player exp */
